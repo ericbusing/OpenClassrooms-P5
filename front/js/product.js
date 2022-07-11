c2fa5fbd5315ebea74ec10f7e92af83e2257a9d2
Eric Busing <busing.eric@gmail.com>
2022-07-11 10:14:57 +0200

    tentative création fonction setHTML et d'appel de celle-ci

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,31 @@
-// Recuperation de l'ID produit.
-let params = new URL(document.location).searchParams;
-let id = params.get("id");
-// Affichage de l'ID, correspondant au produit, dans la console.
-console.log(id);
+/**********VARIABLES**********/
 
-// Creation de constantes.
-const elementImage = document.getElementsByClassName("item__img");
-const elementName = document.getElementById("title");
-const elementPrice = document.getElementById("price");
-const elementDescription = document.getElementById("description");
-const elementColorChoice = document.getElementById("colors")
+// Creation des variables.
+let elementImage = document.getElementsByClassName("item__img");
+let elementName = document.getElementById("title");
+let elementPrice = document.getElementById("price");
+let elementDescription = document.getElementById("description");
+let elementColorChoice = document.getElementById("colors")
 
-const getElement = function () {
-    // Recuperation des donnees sur l'API.
+/**********FONCTIONS**********/
+
+/**
+ * Recuperation de l'ID.
+ * @return {id} identifiant du produit voulu.
+ */
+const getIdFromUrl = function () {
+    let params = new URL(document.location).searchParams;
+    let id = params.get("id");
+    return id;
+}
+
+/** 
+ * Recuperation des donnees sur l'API.
+ * @param {string} "identifiant du produit"
+ * @return {json}
+*/
+const getElement = function (id) {
+
     // Ajout de l'ID sur l'URL.
     fetch(`http://localhost:3000/api/products/${id}`)
         .then(function (response) {
@@ -26,27 +39,48 @@ const getElement = function () {
         .then(function (article) {
             // Affichage du produit voulu dans la console.
             console.log(article);
-            // Placement des donnees API  aux bons endroits.
             elementImage.src = article.imageUrl;
-            elementName.innerText = article.name;
-            elementPrice.innerText = article.price;
-            elementDescription.innerText = article.description;
+            elementName.textContent = article.name;
+            elementPrice.textContent = article.price;
+            elementDescription.textContent = article.description;
+            console.log(article.colors);
+            setColorSelect(article.colors);
+            // console.log(article.article);
+            // setHTML(article.article);
         })
 }
 
-getElement();
+/**
+ * 
+ * 
+ */
+// const setHTML = function (article) {
+//     // Placement des donnees API aux bons endroits.    
+//     elementImage.src = article.imageUrl;
+//     elementName.textContent = article.name;
+//     elementPrice.textContent = article.price;
+//     elementDescription.textContent = article.description;
+// }
 
-const getColor = function () {
-    // Ajout du choix de couleur.
-    let colors = document.getElementById("colors");
-    // // Affichage des couleurs dans la console.
-    // console.log(colorChoice);
-    // Placement des couleurs dans la liste "Choisir une couleur".
-    // elementColorChoice.innerText = article.colors;
+/** 
+ * Ajout des couleurs dans la liste.
+ * @param {array}
+*/
+const setColorSelect = function (colors) {
+    let domColors = document.getElementById("colors");
+    // Affichage des couleurs dans la console.
+    console.log(domColors);
     for (let color of colors) {
-        console.log(`La couleur du canapé est ${color}`);
+        let option = document.createElement("option");
+        option.textContent = color;
+        domColors.appendChild(option);
     }
 }
 
-getColor();
+/**********EVENEMENTS**********/
 
+// Chargement de page
+let productId = getIdFromUrl();
+console.log(productId);
+let productElement = getElement(productId);
+console.log(productElement);
